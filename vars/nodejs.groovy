38cc95c9950719de9b0668450cc3a6b1c30307fe
Sush <sushcloud12@gmail.com>
2023-04-19 19:54:57 +0100

    i

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -10,7 +10,15 @@ sh '''
 } 
 // true is used to mark the present step as pass and skip to next step  even if lint check fails.........
 
-
+def sonarcheck() {
+    sh '''
+    mvn clean compile
+    sonar-scanner -Dsonar.host.url=http://172.31.18.231:9000 -Dsonar.sources=. -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT} 
+    curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > /tmp/quality-gate.sh 
+    chmod +x /tmp/quality-gate.sh && /tmp/quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.18.231 ${COMPONENT}
+    '''
+} 
+// not required to compile for nodejs 
 
 
 
@@ -18,6 +26,9 @@ sh '''
 def call (){ // call is the default which will be called
 pipeline {
     agent any
+    environment {                     
+        SONAR = credentials('sonar') 
+    }
     stages{
         // should run on every coomit to feautre branch 
         stage('Lint Check'){
@@ -26,7 +37,21 @@ pipeline {
                 lintcheck()
 
             }
-             
+            }
+        }
+             stage('Sonar scan'){
+            steps{
+                 script {                     // all fuctions called from groovy file should be placed inside scrpts
+                 sonarcheck()                 // cant declare any shell command in scripts only use groovy based commands
+
+            }
+
+        }
+    }
+
+    stage('build'){
+            steps{
+                 sh "echo This a build stage"
 
         }
     }
