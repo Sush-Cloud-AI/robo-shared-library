ca8a221a8e7d4372c85316b3868bccff4daa8daf
Sush <sushcloud12@gmail.com>
2023-04-19 19:42:31 +0100

    i

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -18,9 +18,11 @@ def sonarcheck() {
     curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > /tmp/quality-gate.sh 
     chmod +x /tmp/quality-gate.sh && /tmp/quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.18.231 ${COMPONENT}
     '''
-
 } 
-
+// 1) for compile require lanugaes like java we need to compile before sonar scan
+//2) ip is the sonar qube host ip with 9000 port of it and the pathof the binary should be passed after the compile
+// 3) sonar scan must me installed on worker node for the scan to work
+//4) for the piple to recognize if the sonar alaysis has failed we use an api which is preent in the sheel script quality gate 
 
 
 
