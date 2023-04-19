7d94b15c542b2cf65ecee6bf5b5822adcdf1698b
Sush <sushcloud12@gmail.com>
2023-04-19 15:36:29 +0100

    i

---
+++ vars/python.groovy
@@ -0,0 +1,38 @@
+// for python based services // shipping //
+
+
+def lintcheck(){
+sh '''
+ echo Lint check starting for  ${COMPONENT}
+ pylint *.py || true
+ echo Lint check completed for  ${COMPONENT}
+ '''    
+} 
+// true is used to mark the present step as pass and skip to next step  even if lint check fails.........
+// pylint *.py  is the tool for lint check for java services
+// install pylint on worker node
+
+
+
+
+
+
+
+def call (){ // call is the default which will be called
+pipeline {
+    agent any
+    stages{
+        // should run on every coomit to feautre branch 
+        stage('Lint Check'){
+            steps{
+                 script {                     // all fuctions called from groovy file should be placed inside scrpts
+                lintcheck()
+
+            }
+             
+
+        }
+    }
+}
+}
+}
